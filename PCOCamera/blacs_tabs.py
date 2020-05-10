02118e1bf79e61a4f3e3c126c1f46d926f6d4b0d
Zak V <zakven@mit.edu>
2020-05-10 02:24:45 -0400

    Updated PCO Camera blacs tab to remember settings for autoranging x and y axes.

--- PCOCamera/blacs_tabs.py
+++ PCOCamera/blacs_tabs.py
@@ -1,3 +1,7 @@
+import ast
+
+import numpy as np
+
 from labscript_devices.IMAQdxCamera.blacs_tabs import IMAQdxCameraTab
 
 
@@ -5,3 +9,58 @@ class PCOCameraTab(IMAQdxCameraTab):
 
     # Override worker class.
     worker_class = 'userlib.user_devices.RbLab.PCOCamera.blacs_workers.PCOCameraWorker'
+
+    def get_save_data(self):
+        save_data = super().get_save_data()
+
+        # Save info about plot settings if an image is plotted.
+        image = self.image.image
+        if image is not None:
+            # Save the shape and dtype of the image.
+            save_data['image_shape'] = repr(image.shape)
+            save_data['image_dtype'] = str(image.dtype)
+
+            # Get the view_box to access its settings.
+            view_box = self.image.getImageItem().getViewBox()
+
+            # Save x and y ranges.
+            targetRange_x, targetRange_y = view_box.targetRange()
+            save_data['targetRange_x'] = targetRange_x
+            save_data['targetRange_y'] = targetRange_y
+
+            # Save whether the x and y ranges are automatically adjusted.
+            autoRange_x, autoRange_y = view_box.autoRangeEnabled()
+            save_data['autoRange_x'] = autoRange_x
+            save_data['autoRange_y'] = autoRange_y
+
+            # TODO: color scale limits?
+            # state = self.image.getImageItem().getViewBox().state
+
+        return save_data
+
+    def restore_save_data(self, save_data):
+        super().restore_save_data(save_data)
+
+        # Restore plot settings if they were saved.
+        if 'image_shape' in save_data:
+            # We have to display some image, otherwise the plot settings will be
+            # overwritten once the first image is displayed. Restore a mostly
+            # blank image of the saved size.
+            image_shape = ast.literal_eval(save_data['image_shape'])
+            image_dtype = np.dtype(save_data['image_dtype'])
+            dummy_image = np.zeros(image_shape, dtype=image_dtype)
+            # Make one pixel nonzero so histogram binning doesn't error.
+            dummy_image[0, 0] = 1
+            self.image.setImage(dummy_image)
+
+            # Restore x and y ranges.
+            view_box = self.image.getImageItem().getViewBox()
+            view_box.setRange(
+                xRange=save_data['targetRange_x'],
+                yRange=save_data['targetRange_y'],
+            )
+
+            # Restore whether the x and y ranges are automatically adjusted.
+            autoRange_x = save_data['autoRange_x']
+            autoRange_y = save_data['autoRange_y']
+            view_box.enableAutoRange(x=autoRange_x, y=autoRange_y)
